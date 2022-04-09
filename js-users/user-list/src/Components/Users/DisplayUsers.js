a9f7543be44b9d5ac54a7240cc71645752d48c34
sshifty <atiszon@gmail.com>
2022-04-09 13:01:09 +0200

    feat: Fetch users from server

--- js-users/user-list/src/Components/Users/DisplayUsers.js
+++ js-users/user-list/src/Components/Users/DisplayUsers.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import SingleUser from "./SingleUser";
+
 const DisplayUsers = () => {
+  const [users, setUsers] = useState(null);
+  const [currentUsers, setCurrentUsers] = useState(null);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const data = await fetch(
+          "https://assessment-users-backend.herokuapp.com/users"
+        );
+        const fetchedData = await data.json();
+        setUsers(fetchedData);
+      } catch (e) {
+        //do something when there is a network error
+        console.log(e);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return <div></div>;
 };
 
