1ad991ce65149e2cb0a7965b3ff5d4e7539c0802
sshifty <atiszon@gmail.com>
2022-04-11 21:09:50 +0200

    feat: Add mock component These Components has no functionality! Only serve one purpose: to make this assesment look like a real app

---
+++ js-users/user-list/src/Components/MockComponents/Nav.js
@@ -0,0 +1,7 @@
+import styles from "./Mock.module.css";
+
+const Nav = () => {
+  return <div ></div>;
+};
+
+export default Nav;
---
+++ js-users/user-list/src/Components/MockComponents/SideMenu.js
@@ -0,0 +1,11 @@
+import styles from './Mock.module.css';
+
+const Sidemenu = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Sidemenu;
